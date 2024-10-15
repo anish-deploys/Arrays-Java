9ca93a01a7b6ae920deae45a926eaa7c7b46b019
Anish Bakshi <117637218+anish-deploys@users.noreply.github.com>
2024-10-15 22:03:25 +0530

    Add files via upload

---
+++ setMatrixZeroes.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+
+public class setMatrixZeroes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows: ");
+        int m = sc.nextInt();
+        System.out.println("Enter the number of columns: ");
+        int n = sc.nextInt();
+
+        int[][] matrix = new int[m][n];
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Flags to check if the first row and first column need to be zeroed
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // Check if the first row has any zeroes
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column has any zeroes
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and first column as markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // Mark the start of the row
+                    matrix[0][j] = 0; // Mark the start of the column
+                }
+            }
+        }
+
+        // Zero out cells based on the markers
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out the first row if needed
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out the first column if needed
+        if (firstColZero) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        System.out.println("Modified matrix:");
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
